16ad98718da660a2601782f3a7c9ca47c6fe3ea3
reapershade <128337478+ReaperShade@users.noreply.github.com>
2024-10-24 11:04:16 -0400

    Add files via upload

---
+++ 21. GRE (Generic Routing Encapsulation).c
@@ -0,0 +1,14 @@
+GRE (Generic Routing Encapsulation)
+
+ 
+	 /*Generic Routing Encapsulation (GRE) is a tunneling protocol that can encapsulate many protocols inside IP tunnels. Some examples would be routing protocols such as EIGRP and OSPF and the routed protocol IPv6.
+	 
+	 As the data is passed down each layer of the model, more information containing details specific to the layer in question is added on to the start of thE transmission.
+	 Notice that the encapsulated data is given a different name at different steps of the process. In layers 7,6 and 5, the data is simply referred to as data. In the transport layer 
+	 the encapsulated data is referred to as a segment or a datagram (depending on whether TCP or UDP has been selected as a transmission protocol).
+	 At the Network Layer, the data is referred to as a packet. When the packet gets passed down to the Data Link layer it becomes a frame, and by the time its transmitted across a network the frame has been broken down into bits.
+
+
+
+	 
+
