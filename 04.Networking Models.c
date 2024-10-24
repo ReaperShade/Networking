351959905b878fd22dc5d5fe3f1ac4c8eb785ac9
reapershade <128337478+ReaperShade@users.noreply.github.com>
2024-10-24 09:50:38 -0400

    Add files via upload

---
+++ 04.Networking Models.c
@@ -0,0 +1,143 @@
+1. THE OPEN SYSTEM INTERCONNECTION MODEL (OSI)
+
+ 
+
+		/*One of the greatest functions of the OSI specifications is to assist in data transfer between disparate hosts regardless of whether they’re Unix/Linux, Windows, or macOS-based. But keep  in mind that the OSI model isn’t a physical model; it’s a conceptual and comprehensive yet fluid set of guidelines, that application developers utilize to create and implement  applications that run on a network. It also provides a framework for creating and implementing networking standards, devices.*/
+
+
+
+
+
+
+
+
+
+
+
+
+2. MODEL OSI LAYERS
+
+ 
+ 		(Upper Layers)
+ 
+ 		- Application      	  //File, print, message, database, and application services (dns,)
+ 		- Presentation		  //Data encryption, compression, and translation services (ssl, tls)
+ 		- Session		      //control protocols, tunneling protocols							
+ 
+ 
+ 		(Lower layers)
+ 
+ 		- Transport		      //tcp segment, udp datagram
+ 		- Network             //Ip addresses, Router, packet 
+ 		- Data Link           //Frame, mac address, extended unique identifier, switch
+ 		- Physical            //Cables, Fiber, and the signal itself
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 3. PHYSICAL LAYER 
+
+		//The Physical layer specifies the electrical, mechanical, procedural, and functional requirements for activating, maintaining, and deactivating a physical link between end systems.
+
+
+
+
+
+
+
+
+
+
+
+
+ 4. DATA LINK LAYER 
+
+ 
+ 		/*The Data Link layer provides the physical transmission of the data and handles error notification, network topology, and flow control. his means the Data Link layer ensures that messages are delivered to the proper device on a LAN using hardware ==(MAC) addresses== and translates messages from the Network layer Into bits for the Physical layer to transmit.*/
+
+
+
+
+
+
+
+
+
+
+
+ 5. NETWORK LAYER 
+
+ 
+ 		/*The Network layer manages logical device addressing, tracks the location of devices on the network, and determines the best way to move data. This means that the Network layer must transport traffic between devices that aren’t locally attached. Routers are layer 3 devices that are specified at the Network layer and provide the routing services within an internetwork.
+ 	
+ 		Data Packets These are used to transport user data through the internetwork. Protocols used to support data traffic are called routed protocols. Two examples of routed protocols are ==Internet Protocol (IP) and  Internet Protocol version 6 (IPv6). */
+
+
+
+
+
+
+
+
+
+
+
+ 6. TRANSPORT LAYER 
+
+ 
+ 		/*The Transport layer segments and reassembles data into a data stream. Services located in the Transport layer handle data from upper-layer applications and unite it onto the same data stream. Data integrity is ensured at the Transport layer by maintaining flow control and by allowing users to request reliable data transport between systems. Flow control provides a means for the receiver to govern the amount of data sent by the sender.*/
+ 
+
+
+
+
+
+
+
+
+
+
+ 7.  SESSION LAYER 
+
+ 
+ 		/*The Session layer is responsible for setting up, managing, and then tearing down sessions between Presentation layer entities. This layer also provides dialog control  between devices, or nodes. It coordinates communication between systems and serves to organize their communication by offering three different modes: one direction (simplex), both directions, but only one direction at a time half-duplex),and bi-directional (full-duplex).*/
+
+
+
+
+
+
+
+
+
+
+
+ 8. PRESENTATION LAYER 
+
+ 
+ 		/*The Presentation layer gets its name from its purpose: it presents data to the Application layer and is responsible for data translation and code formatting. */
+
+
+
+
+
+
+
+
+
+
+
+ 9. APPLICATION LAYER 
+
+ 
+ 		/*The Application layer of the OSI model marks the spot where users actually communicate or interact with the computer. Technically, users communicate with the network stack through application processes, interfaces, or application programming interfaces (APIs) that connect the application in use to the operating system of the computer. The Application layer chooses and determines the availability of communicating partners along with the resources necessary to make their required connections*/
