43e92a4c36518e0c726e4db4b2e882ab74e517cd
reapershade <128337478+ReaperShade@users.noreply.github.com>
2024-10-24 11:03:51 -0400

    Add files via upload

---
+++ 20.RARP.c
@@ -0,0 +1,5 @@
+RARP (Reverse address resolution protocol)
+
+ 
+ 	//When an IP machine happens to be a diskless machine, it has no way of initially knowing its IP address. But it does know its MAC address. Reverse Address Resolution Protocol (RARP) discovers the identity of the IP address for diskless machines by sending out a packet that includes its MAC address and a request for the IP address assigned to that MAC address. A designated machine, called a RARP server, responds with the answer, and the identity crisis is over. RARP uses the information it does know about the machine’s MAC address to learn its IP address and complete the machine’s ID portrait.
+
