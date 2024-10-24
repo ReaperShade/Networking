3930780a61110588d654e6c729875a9cb42dcaa5
reapershade <128337478+ReaperShade@users.noreply.github.com>
2024-10-24 10:55:10 -0400

    Add files via upload

---
+++ 17. Wireless.c
@@ -0,0 +1,39 @@
+1 WIRELESS NETWORK 
+
+
+		//Wireless networks are computer networks that utilize wireless data connections among network nodes. They enable devices like laptops, smartphones, and tablets to communicate with one another and connect to the Internet without the need for physical connections like cables.
+
+		//These networks rely on radio frequency (RF) technology to transmit data between devices. Each device in a wireless network is equipped with a wireless adapter that converts data into RF signals and transmits them wirelessly.
+
+		//Communication between devices occurs over RF in the 2.4 GHz or 5 GHz bands in a WiFi network. When a device, like a laptop, wants to send data over the network, it first communicates with the Wireless Access Point (WAP) to request permission to transmit.
+
+
+
+
+
+
+
+
+2. WIFI CONNECTION 
+
+
+
+		//The device must also be configured with the correct network settings, such as the network name / Service Set Identifier (SSID) and password. So, to connect to the router, the laptop uses a wireless networking protocol called IEEE 802.11. This protocol defines the technical details of how wireless devices communicate with each other and with WAPs.
+		//When a device wants to join a WiFi network, it sends a request to the WAP to initiate the connection process. This request is known as a connection request frame or association request and is sent using the IEEE 802.11 wireless networking protocol.
+
+		//The device then uses this information to configure its wireless adapter and connect to the WAP. Once the connection is established, the device can communicate with the WAP and other network devices. 
+
+
+
+
+
+
+
+3. WEP CHALLENGE-RESPONSE HANDSHAKE 
+
+
+
+		//The challenge-response handshake is a process to establish a secure connection between a WAP and a client device in a wireless network that uses the WEP security protocol. This involves exchanging packets between the WAP and the client device to authenticate the device and establish a secure connection.
+
+
+		//We can use various encryption algorithms to protect the confidentiality of data transmitted over wireless networks. The most common encryption algorithms in WiFi networks are Wired Equivalent Privacy (WEP), WiFi Protected Access 2 (WPA2), and WiFi Protected Access 3 (WPA3).
