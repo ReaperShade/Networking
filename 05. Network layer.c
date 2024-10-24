d40f3cdaf3707a92da7ce97d2e1b706743138e43
reapershade <128337478+ReaperShade@users.noreply.github.com>
2024-10-24 09:57:16 -0400

    Add files via upload

---
+++ 05. Network layer.c
@@ -0,0 +1,16 @@
+1. NETWORK LAYER IN TCP/IP MODEL
+
+
+
+		//The network layer (Layer 3) of the OSI model manages the exchange of data packets, as they cannot be directly sent to the receiver and must be routed through intermediary nodes. These packets are transferred from one node to the next until they reach their destination. To facilitate this, the network layer identifies individual network nodes, establishes and terminates connection paths, and handles routing and data flow control. During transmission, addresses are analyzed, and data is routed through the network node by node. Typically, data is not processed in layers above Layer 3 within these nodes. Routing and the creation of routing tables are based on the packet addresses.
+
+
+
+		//Protocols are defined in each layer of OSI, and these protocols represent a collection of rules for communication in the respective layer. They are transparent to the protocols of the layers above or below. Some protocols fulfill tasks of several layers and extend over two or more layers. The most used protocols on this layer are:
+
+		    IPv4 / IPv6
+		    IPsec
+		    ICMP
+		    IGMP
+		    RIP
+		    OSPF
