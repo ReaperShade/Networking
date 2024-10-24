e2683c112bec7a4b0344a93a19d21ea44eb9cfc1
reapershade <128337478+ReaperShade@users.noreply.github.com>
2024-10-24 10:13:44 -0400

    Add files via upload

---
+++ 07. Broadcast vs Unicast.c
@@ -0,0 +1,28 @@
+1. BROADCAST VS UNICAST 
+
+ 
+
+	 	/*Broadcast and unicast are two communication methods used in computer networks to transmit data between devices.*/
+	 	
+	 	- Broadcast: /*In a broadcast communication, a single message is sent from a source device to all devices on the network. The message is typically addressed to a special broadcast address that represents
+	 			     all devices on the network. When a device receives a broadcast message, it processes the message if it is intended for it or simply ignores it otherwise. Broadcast communication is commonly used for network discovery, service announcements, and distributing information to all devices on the network simultaneously. However, it can consume network bandwidth and processing resources of all devices, even if they are not interested in the message.*/
+
+	 	
+	 	- Unicast: /*Unicast communication involves sending a message from a source device to a specific destination device. The source device explicitly addresses the message to the intended recipient using its 
+	 			   unique identifier, such as an IP address or a MAC address. Only the designated device receives and processes the message. Unicast communication is typically used for point-to-point communication where a specific device needs to receive a message. It ensures that the message reaches only the intended recipient, minimizing network congestion and resource utilization.*/
+	 	
+
+
+
+
+
+
+
+	 	To summarize:
+
+	 	- Broadcast communication sends a message to all devices on the network, whereas unicast communication sends a message to a specific device.
+	 	- Broadcast communication is suitable for scenarios where a message needs to be distributed to multiple devices simultaneously.
+	 	- Unicast communication is used when a message is intended for a specific device, ensuring efficient and targeted communication.
+	 	
+
+	 	/*It's worth noting that there are other communication methods like multicast and any cast, which are variations of broadcast and unicast, respectively, with their own specific characteristics and use cases.
